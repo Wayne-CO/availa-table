3eeac904f5a09751f0fd8b286f2537d7f3c3798b
Wayne Huang <wayne@craftedorigin.com>
2024-01-05 22:53:59 -0800

    fetches all restaurants
    * in terminal

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Button from "@mui/material/Button";
+import { prisma } from "lib/prisma";
 
-export default function Home() {
+const fetchRestaurants = async () => {
+  const restaurants = await prisma.restaurant.findMany();
+
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
+  console.log("restaurants :>> ", restaurants);
   return (
     <div>
       <Button variant="contained">Hello</Button>
