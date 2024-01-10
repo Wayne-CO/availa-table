09cb37691a1ac6b09a2a96884c451c53e7b87185
Wayne Huang <wayne@craftedorigin.com>
2024-01-10 12:57:03 -0800

    adds config and example component for sb

---
+++ stories/MuiButton.stories.tsx
@@ -0,0 +1,18 @@
+import Button from "./MuiButton";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { title: "Example/MuiButton", component: Button };
+export interface ButtonProps {
+  label: string;
+}
+
+// export const Button = ({ label, ...rest }: ButtonProps) => (
+//   <MuiButton {...rest}>{label}</MuiButton>
+// );
+
+export const Primary = {
+  args: {
+    label: "hi",
+    variant: "contained",
+  },
+};
