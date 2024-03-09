595f41ef6b159fe6c3100308fec4fb546367273e
Wayne Huang <wayne@craftedorigin.com>
2024-03-09 10:50:48 -0800

    fetches restaurant in console

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,32 @@
-export default function RestaurantDetails() {
-  return <div>RestaurantDetails</div>;
+import { Box } from "@mui/material";
+import { prisma } from "@/lib/prisma";
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUniqueOrThrow({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  return restaurant;
+};
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function RestaurantDetails({ params }: Props) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
+  console.log("restaurant", restaurant);
+  return <Box></Box>;
 }
