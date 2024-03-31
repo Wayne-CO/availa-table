acbbc9d368c31e286a2eea295f0ed59ee8600fe0
Wayne Huang <wayne@craftedorigin.com>
2024-03-30 17:20:28 -0700

    searches for restaurants by city

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,39 @@
-export default function Search() {
+import { prisma } from "@/lib/prisma";
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    mainImage: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  // !Return all restaurants if no city is provided
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLocaleLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+type Props = {
+  searchParams: {
+    city: string;
+  };
+};
+
+export default async function Search({ searchParams }: Props) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+  console.log(restaurants);
   return <div>Search</div>;
 }
