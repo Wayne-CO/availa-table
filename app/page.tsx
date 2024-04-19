9a39bb5833fd746bb506f967fd77e15147e2e4f3
Wayne Huang <wayne@craftedorigin.com>
2024-04-19 15:13:32 -0700

    adds logo and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { prisma } from "lib/prisma";
 import RestaurantCard from "./components/RestaurantCard";
 
+import avaliaTableLogo from "../public/availatable-logo.svg";
 import bannerMain from "../public/banner-main.svg";
 
 type RestaurantsCardData = Prisma.PromiseReturnType<typeof fetchRestaurants>;
@@ -59,14 +60,34 @@ export default async function Home() {
         />
       </Box>
 
-      <Box component="main" width="1272px" margin="auto" px="20px" pt="646px">
-        <Grid container spacing={2}>
-          {restaurants.map((restaurant) => (
-            <Grid key={restaurant.id}>
-              <RestaurantCard restaurant={restaurant} />
-            </Grid>
-          ))}
-        </Grid>
+      <Box component="main" width="1272px" margin="auto" px="20px">
+        <Box display="flex">
+          <Image alt="Availatable" src={avaliaTableLogo} />
+        </Box>
+
+        <Typography
+          variant="h1"
+          fontSize="60px"
+          color="#FFFFFF"
+          fontWeight="200"
+          pt="36px"
+          textAlign="center"
+        >
+          AVAILA
+          <Typography component="span" fontSize="inherit" fontWeight="900">
+            TABLE
+          </Typography>
+        </Typography>
+
+        <Box component="main" pt="646px">
+          <Grid container spacing={2}>
+            {restaurants.map((restaurant) => (
+              <Grid key={restaurant.id}>
+                <RestaurantCard restaurant={restaurant} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       </Box>
     </Box>
   );
