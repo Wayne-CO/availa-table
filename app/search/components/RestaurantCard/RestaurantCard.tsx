0f5717e5f697c4af60778cbcb853ba0f647ea2b9
Wayne Huang <wayne@craftedorigin.com>
2024-09-06 19:56:42 -0700

    card styling

--- app/search/components/RestaurantCard/RestaurantCard.tsx
+++ app/search/components/RestaurantCard/RestaurantCard.tsx
@@ -22,57 +22,62 @@ export default function RestaurantCard({ restaurant }: Props) {
   return (
     <Card>
       <CardActionArea LinkComponent={Link} href={`/restaurant/${slug}`}>
-        <CardMedia sx={{ height: "127px" }} image={mainImage} />
-        <CardContent
-          sx={{
-            minHeight: "130px",
-            pb: "16px",
-          }}
-        >
-          <Typography
-            gutterBottom
-            variant="h3"
-            fontSize={theme.typography.pxToRem(24)}
-            textTransform="capitalize"
-            textOverflow="ellipsis"
-            overflow="hidden"
-            whiteSpace="nowrap"
-            mb="2px"
+        <Box display="flex">
+          <CardMedia
+            sx={{ width: "172px", height: "148px", m: "10px" }}
+            image={mainImage}
+          />
+          <CardContent
+            sx={{
+              minHeight: "130px",
+              pb: "16px",
+            }}
           >
-            {name}
-          </Typography>
+            <Typography
+              gutterBottom
+              variant="h3"
+              fontSize={theme.typography.pxToRem(24)}
+              textTransform="capitalize"
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              mb="2px"
+            >
+              {name}
+            </Typography>
 
-          <Typography
-            fontSize={theme.typography.pxToRem(14)}
-            mb="2px"
-            color="text.secondary"
-            textOverflow="ellipsis"
-            overflow="hidden"
-            whiteSpace="nowrap"
-            textTransform="capitalize"
-          >
-            {location.name} • {cuisine.name}
-          </Typography>
+            <Typography
+              fontSize={theme.typography.pxToRem(14)}
+              mb="2px"
+              color="text.secondary"
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              textTransform="capitalize"
+            >
+              {location.name} • {cuisine.name}
+            </Typography>
 
-          <Box display="flex" alignItems="center" pb="2px">
-            <Box>
-              <Typography pr="5px">5</Typography>
+            <Box display="flex" alignItems="center" pb="2px">
+              <Box>
+                <Typography pr="5px">5</Typography>
+              </Box>
+              <Box pr="5px">*****</Box>
+              <Box>
+                <Typography color="text.secondary" pr="3px" variant="body2">
+                  (1,234) •
+                </Typography>
+              </Box>
+              <Box>
+                <Price price={price} />
+              </Box>
             </Box>
-            <Box pr="5px">*****</Box>
-            <Box>
-              <Typography color="text.secondary" pr="3px" variant="body2">
-                (1,234) •
-              </Typography>
-            </Box>
-            <Box>
-              <Price price={price} />
-            </Box>
-          </Box>
 
-          <Typography color="text.secondary" variant="body2">
-            Guests who booked today: 100
-          </Typography>
-        </CardContent>
+            <Typography color="text.secondary" variant="body2">
+              Guests who booked today: 100
+            </Typography>
+          </CardContent>
+        </Box>
       </CardActionArea>
     </Card>
   );
