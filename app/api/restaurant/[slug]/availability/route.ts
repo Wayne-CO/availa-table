a1a1e5245d7b0e2cef5268dd50a53c9199631e2a
Wayne Huang <wayne@craftedorigin.com>
2024-11-17 22:54:58 -0800

    retrieve the search times

--- app/api/restaurant/[slug]/availability/route.ts
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest } from "next/server";
+import { times } from "@/app/data";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { slug: string } },
+  // { params }: { params: { slug: string } },
 ) {
-  const { slug } = params;
   const searchParams = req.nextUrl.searchParams;
   const day = searchParams.get("day");
   const time = searchParams.get("time");
@@ -19,5 +19,18 @@ export async function GET(
     );
   }
 
-  return Response.json({ slug, day, time, partySize });
+  const searchTimes = times.find((t) => {
+    return t.time === time;
+  })?.searchTimes;
+
+  if (!searchTimes) {
+    return Response.json(
+      {
+        errorMessage: "Invalid search time provided",
+      },
+      { status: 400 },
+    );
+  }
+
+  return Response.json({ searchTimes });
 }
