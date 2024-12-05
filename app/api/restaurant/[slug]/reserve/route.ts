8df8c4f680d830e129adef68a43d5dc03a434b6c
Wayne Huang <wayne@craftedorigin.com>
2024-12-05 13:37:18 -0800

    adds validation

--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function POST(
   req: NextRequest,
@@ -9,6 +10,33 @@ export async function POST(
   const time = searchParams.get("time");
   const partySize = searchParams.get("partySize");
   const slug = params.slug;
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    return Response.json(
+      {
+        errorMessage: "Invalid restaurant provided",
+      },
+      { status: 400 },
+    );
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.openTime}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.closeTime}`)
+  ) {
+    return Response.json(
+      {
+        errorMessage: "Restaurant is not open at that time",
+      },
+      { status: 400 },
+    );
+  }
 
   return Response.json({ slug, day, time, partySize });
 }
