88693185d79e21b28c579cd5b4ec3c2293d79f23
Wayne Huang <wayne@craftedorigin.com>
2024-12-06 15:59:28 -0800

    determines the tables to book

--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -97,5 +97,35 @@ export async function POST(
     }
   });
 
-  return Response.json({ tablesCount });
+  const tablesToBooks: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    // if there is a 4 seater then take that
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      // if only 2 seats remain
+      if (tablesCount[2].length) {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        // if no 2 seaters remain then give 4 seater
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
+  return Response.json({ tablesCount, tablesToBooks });
 }
