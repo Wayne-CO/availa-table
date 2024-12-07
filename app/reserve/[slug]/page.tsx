e441ab3265fa918f8fd302acda0cad917c15f9d1
Wayne Huang <wayne@craftedorigin.com>
2024-12-06 21:09:13 -0800

    fetches the restaurant by slug

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,3 +1,19 @@
-export default function Reserve() {
-  return <div>Reserve</div>;
+import { prisma } from "@/lib/prisma";
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUniqueOrThrow({
+    where: {
+      slug,
+    },
+  });
+
+  return restaurant;
+};
+export default async function Reserve({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  return <div>{JSON.stringify(restaurant)}</div>;
 }
