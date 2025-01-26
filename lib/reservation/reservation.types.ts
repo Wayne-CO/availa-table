508b08da61c4b97f1303e151af62a67c0fdf435b
Wayne Huang <wayne@craftedorigin.com>
2025-01-25 21:43:23 -0800

    adds validation

--- lib/reservation/reservation.types.ts
+++ lib/reservation/reservation.types.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
 export const reservationRequestSchema = z.object({
-  bookerFirstName: z.string(),
-  bookerLastName: z.string(),
+  bookerFirstName: z
+    .string()
+    .min(2, { message: "First name requires at least 2 characters." }),
+  bookerLastName: z
+    .string()
+    .min(2, { message: "Last name requires at least 2 characters." }),
   bookerPhone: z.string(),
-  bookerEmail: z.string(),
+  bookerEmail: z
+    .string()
+    .email({ message: "Please provide a valid email address." }),
   bookerOccasion: z.string().nullish(),
   bookerRequest: z.string().nullish(),
 });
